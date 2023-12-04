901c7b991a419f680ae4fa9ad539a65b0f92aa5c
danvi02 <147135182+danvi02@users.noreply.github.com>
2023-12-04 18:25:57 +0530

    Update restaurants.ts

--- src/db/restaurants.ts
+++ src/db/restaurants.ts
@@ -1,130 +1,82 @@
 export const RESTAURANTS = [
 	{
 	"restaurant_id": 1,
-	"restaurant_name": "Srikanya",
+	"restaurant_name": "Bakingo",
 	"rating": 4.2,
-	"menuType": ["Biryani", "North Indian", "Chinese"],
+	"menuType": ["Bakery"],
 	"url": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/9fc6c247e09254670265953d03701280",
-	"location": "Serilingampally"
+	"location": "Gachibowli"
 	},
 	{
 	"restaurant_id": 2,
-	"restaurant_name": "Pista House",
-	"rating": 3.2,
-	"menuType": ["Biryani", "Chinese"],
+	"restaurant_name": "NH1 Bowls Restaurant",
+	"rating": 4.4,
+	"menuType": ["North Idian", "Punjabi", "Chinese", "Snacks"],
 	"url": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/94654fdf308764cd0faf83dba35bcdc3",
 	"location": "Kukatpally"
 	},
     {
         "restaurant_id": 3,
-        "restaurant_name": "Spice Junction",
-        "rating": 4.0,
-        "menuType": ["Indian", "Mughlai", "South Indian"],
+        "restaurant_name": "Mojo Pizza",
+        "rating": 3.9,
+        "menuType": ["Pizza", "Burgers"],
 		"url": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/59b64f9bb67234987652437f21d379c2",
         "location": "Hitech City"
     },
     {
         "restaurant_id": 4,
-        "restaurant_name": "Taj Mahal Delight",
+        "restaurant_name": "Box8-Desi Meals",
         "rating": 3.9,
-        "menuType": ["Mughlai", "Kebabs", "Biryani"],
+        "menuType": ["North Indian", "Biriyanis,", "Thalis"],
 		"url": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/69a061b7e0f951cef2b4947946f94ec6",
         "location": "Banjara Hills"
     },
     {
         "restaurant_id": 5,
-        "restaurant_name": "LunchBox",
+        "restaurant_name": "Faasoos - Wraps & Rolls",
         "rating": 3.8,
-        "menuType": ["Meals", "Thali"],
+        "menuType": ["Kababs", "Fast Food", "Snacks", "North Indian"],
 		"url": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/af33b81798b11deba338e94b7585d348",
-        "location": "Madhapur"
+        "location": "Lingampally"
     },
     {
         "restaurant_id": 6,
-        "restaurant_name": "Café de Lights",
-        "rating": 4.3,
-        "menuType": ["Continental", "Italian", "Coffee"],
-		"url": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/oq6ufq59p49s0ka7tozm",
-        "location": "Jubilee Hills"
+        "restaurant_name": "Warm Oven Cakes",
+        "rating": 3.9,
+        "menuType": ["Bakery", "Deserts", "Ice Cream"],
+		"url": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e938fb5f416cc2c28b4b519cf27b04cc",
+        "location": "Nalagandla"
     },
     {
         "restaurant_id": 7,
-        "restaurant_name": "Dosa Delights",
-        "rating": 4.1,
-        "menuType": ["South Indian", "Vegetarian"],
-		"url": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/20e1100c97af1f0caf697414fd2adb5f",
-        "location": "Gachibowli"
+        "restaurant_name": "Louis Burger",
+        "rating": 4.2,
+        "menuType": ["Burgers", "American", "Fast Food"],
+		"url": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/epdeei1h2gppxwthw0cd",
+        "location": "Huda Layout"
     },
     {
         "restaurant_id": 8,
-        "restaurant_name": "Mojo Pizza",
-        "rating": 4.4,
-        "menuType": ["Pizza", "Italian", "Garlic Bread"],
-		"url": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/epdyw6ijcvmdnjlbylzm",
+        "restaurant_name": "K Grill Restaurant",
+        "rating": 4.1,
+        "menuType": ["Biriyani", "Chinese", "North Indian"],
+		"url": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/wmymeeipprnxfmi0jv68",
         "location": "Ameerpet"
     },
     {
         "restaurant_id": 9,
-        "restaurant_name": "Sizzling Sizzlers",
-        "rating": 3.9,
-        "menuType": ["Sizzlers", "Continental", "Steak"],
-		"url": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/xvk2jlbijyejf09fdqkp",
-        "location": "Secunderabad"
+        "restaurant_name": "Natural Ice Creams",
+        "rating": 4.3,
+        "menuType": ["Ice Creams", "Deserts"],
+		"url": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/h1lmgubux6ckbxwjak09",
+        "location": "Tellapur"
     },
     {
         "restaurant_id": 10,
-        "restaurant_name": "Chaat Corner",
+        "restaurant_name": "Sri Kanya",
         "rating": 4.2,
-        "menuType": ["Street Food", "Chaat", "Snacks"],
+        "menuType": ["Biriyanis", "North Indian", "Thalis", "Snacks"],
 		"url": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/xvk2jlbijyejf09fdqkp",
-        "location": "Miyapur"
-    },
-    {
-        "restaurant_id": 11,
-        "restaurant_name": "Rajasthani Rasoi",
-        "rating": 4.0,
-        "menuType": ["Rajasthani", "North Indian", "Thali"],
-		"url": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/xvk2jlbijyejf09fdqkp",
-        "location": "Kondapur"
-    },
-    {
-        "restaurant_id": 12,
-        "restaurant_name": "Seafood Sensation",
-        "rating": 4.5,
-        "menuType": ["Seafood", "Coastal", "Fish Curry"],
-		"url": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/xvk2jlbijyejf09fdqkp",
-        "location": "LB Nagar"
-    },
-    {
-        "restaurant_id": 13,
-        "restaurant_name": "Green Garden Cafe",
-        "rating": 4.1,
-        "menuType": ["Healthy Options", "Salads", "Smoothies"],
-		"url": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/xvk2jlbijyejf09fdqkp",
-        "location": "Manikonda"
-    },
-    {
-        "restaurant_id": 14,
-        "restaurant_name": "Kabab Kingdom",
-        "rating": 4.3,
-        "menuType": ["Kebabs", "Mughlai", "Tandoori"],
-		"url": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/xvk2jlbijyejf09fdqkp",
-        "location": "Chandanagar"
-    },
-    {
-        "restaurant_id": 15,
-        "restaurant_name": "Vegetarian Villa",
-        "rating": 4.0,
-        "menuType": ["Vegetarian", "North Indian", "Thali"],
-		"url": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/xvk2jlbijyejf09fdqkp",
-        "location": "Nampally"
-    },
-    {
-        "restaurant_id": 16,
-        "restaurant_name": "Sweet Treats Bakery",
-        "rating": 4.4,
-        "menuType": ["Bakery", "Desserts", "Cakes"],
-		"url": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/xvk2jlbijyejf09fdqkp",
-        "location": "Attapur"
+        "location": "Kukatpally"
     }
 ]
